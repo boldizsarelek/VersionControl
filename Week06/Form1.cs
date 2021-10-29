0cc07f406681647226ff4c0ebf800f8ef0b76ace
Boldizsár Elek <boldizsar.elek@stud.uni-corvinus.hu>
2021-10-30 00:21:38 +0200

    7. feladat

--- Week06/Form1.cs
+++ Week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Week06.MnbServiceReference;
 
@@ -15,31 +16,39 @@ namespace Week06
     public partial class Form1 : Form
     {
 
-        BindingList<RateData> Rates;
+        BindingList<RateData> Rates = new BindingList<RateData>();
         
         public Form1()
         {
             InitializeComponent();
 
             dataGridView1.DataSource = Rates;
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+
+            Rates.Clear();
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = (string)comboBox1.SelectedItem,
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
 
-            processData(result);
+            ProcessData(result);
+            ShowData();
         }
 
-        private void processData(string result)
+        private void ProcessData(string result)
         {
             var xml = new XmlDocument();
 
@@ -62,7 +71,42 @@ namespace Week06
                     rate.Value = value / unit;
                 }
             }
+        }
 
+        private void ShowData()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
