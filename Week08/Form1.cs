a43d143d8ef57f07ebd5fe7ac53c0e0b71813b5f
Boldizsár Elek <boldizsar.elek@stud.uni-corvinus.hu>
2021-11-12 17:53:35 +0100

    Színválasztó gomb

--- Week08/Form1.cs
+++ Week08/Form1.cs
@@ -79,5 +79,17 @@ namespace Week08
         {
             Factory = new BallFactory();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
