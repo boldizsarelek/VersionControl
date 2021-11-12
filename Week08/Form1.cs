db94f4092a6e9b7066a29510ad52ee220ddf1a71
Boldizsár Elek <boldizsar.elek@stud.uni-corvinus.hu>
2021-11-12 15:59:48 +0100

    Labdagyár létrehozása, futószalag létrehozása

--- Week08/Form1.cs
+++ Week08/Form1.cs
@@ -12,9 +12,47 @@ namespace Week08
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxposition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+
+                if (ball.Left > maxposition)
+                {
+                    maxposition = ball.Left;
+                }
+            }
+            if (maxposition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
