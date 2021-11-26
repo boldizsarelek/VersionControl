2b0ce6d6a0a054f4dcd2dab2e05bf10798f2d8ed
Boldizsár Elek <boldizsar.elek@stud.uni-corvinus.hu>
2021-11-26 19:46:15 +0100

    Register függvény HappyPath

--- Week10/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Week10/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -42,12 +42,35 @@ namespace UnitTestExample.Test
             ]
         public void TestValidatePassword(string password, bool expectedResult)
         {
-
+            //Arrange
             var accountController = new AccountController();
 
+            //Act
             var actualResult = accountController.ValidatePassword(password);
 
+            //Assert
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567")
+        ]
+
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.Register(email, password);
+
+            //Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+
         }
     }
 }
