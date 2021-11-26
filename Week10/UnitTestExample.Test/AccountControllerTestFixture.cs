6d7368793768e0e5416e61fd0c073263daa23ec7
Boldizsár Elek <boldizsar.elek@stud.uni-corvinus.hu>
2021-11-26 18:44:03 +0100

    E-mail validálás teszt függvény

---
+++ Week10/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFixture
+    {
+        [Test]
+        void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
+    }
+}
