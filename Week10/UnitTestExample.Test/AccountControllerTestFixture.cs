459b488587299dd3aa62d0757963ef29b55a0c55
Boldizsár Elek <boldizsar.elek@stud.uni-corvinus.hu>
2021-11-26 19:22:23 +0100

    Jelszó validálás teszt

--- Week10/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Week10/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,8 +10,14 @@ namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
-        [Test]
-        void TestValidateEmail(string email, bool expectedResult)
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-crovinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
         {
             //Arrange
             var accountController = new AccountController();
@@ -20,6 +26,27 @@ namespace UnitTestExample.Test
             var actualResult = accountController.ValidateEmail(email);
 
             //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("Abcdefghij", false),
+            TestCase("ABCDEFGHIJ", false),
+            TestCase("abcdefghij", false),
+            TestCase("Rovid1", false),
+            TestCase("Megfelelo123", true)
+            /*"A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből
+             * és számokból állhat, és tartalmaznia kell legalább egy kisbetűt,
+             * egy nagybetűt és egy számot." */
+            ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
